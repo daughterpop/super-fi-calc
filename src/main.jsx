47b896ac28291f343e9a5dd436eaac3a5ba132e3
dhimmer <dustin.himmerich@protonmail.com>
2026-02-28 11:37:53 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+
+import Home from './pages/Home';
+import FICalculator from './pages/FICalculator';
+import BlogPost from './pages/BlogPost';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculators/fi-calc" element={<FICalculator />} />
+        <Route path="/blog/first-post" element={<BlogPost />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
