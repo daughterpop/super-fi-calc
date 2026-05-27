366c210520eda0a00d5572a5d57572afbe35fbf3
dhimmer <dustin.himmerich@protonmail.com>
2026-05-27 17:37:42 -0500

    Fix App.jsx: Add SuperFiCalculator import and render (remove old broken references)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import SubscribeForm from './components/SubscribeForm';
+import SuperFiCalculator from './Super-Fi-Calculator.jsx';
 
 function App() {
   return (
     <>
+      <SuperFiCalculator />
       <SubscribeForm />
     </>
   );
