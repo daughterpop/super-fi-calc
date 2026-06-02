07ac04b5e82f6848885bb1e7953c701ce2e45e15
dhimmer <dustin.himmerich@protonmail.com>
2026-06-02 16:31:31 -0700

    Fix main.jsx to properly render React app - this should fix blank homepage

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
-// Updated with new import and route
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
