4721138ac27f50b299db39bb3b999b81a16c585b
dhimmer <dustin.himmerich@protonmail.com>
2026-07-29 11:06:56 -0500

    Add Join the list footer

--- src/pages/how-christian-couples-unlock-financial-freedom-together-faith-teamwork-and-a-legacy-your-family-will-thank-you-for.jsx
+++ src/pages/how-christian-couples-unlock-financial-freedom-together-faith-teamwork-and-a-legacy-your-family-will-thank-you-for.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import BlogPostFooter from '../components/BlogPostFooter';
 
 export default function HowChristianCouplesUnlockFinancialFreedomTogetherFaithTeamworkAndALegacyYourFamilyWillThankYouFor() {
   return (
@@ -66,11 +67,7 @@ export default function HowChristianCouplesUnlockFinancialFreedomTogetherFaithTe
           – Dustin
         </p>
 
-        <div className="mt-12 pt-8 border-t border-gray-200">
-          <a href="/" className="text-indigo-600 hover:text-indigo-800 font-medium">
-            ← Back to Home
-          </a>
-        </div>
+        <BlogPostFooter />
       </div>
     </div>
   );
