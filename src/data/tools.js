3998256e6478aea5d5785f35d9aa61224270737d
dhimmer <dustin.himmerich@protonmail.com>
2026-08-07 10:27:00 -0500

    Restore full tools catalog as modular data files + SiteFooter on Tools page

--- src/data/tools.js
+++ src/data/tools.js
@@ -1,1 +1,12 @@
-PLACEHOLDER_WILL_FAIL
+/** Hand-picked stewardship tools + referral perks for /tools */
+import { tools_investing } from './tools_investing';
+import { tools_banking } from './tools_banking';
+import { tools_shopping } from './tools_shopping';
+import { tools_other } from './tools_other';
+
+export const tools = [
+  ...tools_investing,
+  ...tools_banking,
+  ...tools_shopping,
+  ...tools_other,
+];
