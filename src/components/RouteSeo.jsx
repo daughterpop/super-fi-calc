1189dd50eee8944fc37f2bd2330b8483cd2c8c72
dhimmer <dustin.himmerich@protonmail.com>
2026-09-04 10:11:28 -0500

    Restore RouteSeo after accidental overwrite

--- src/components/RouteSeo.jsx
+++ src/components/RouteSeo.jsx
@@ -1,1 +1,11 @@
-see file
+import { useLocation } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+import { getPostByPath, allPosts } from '../data/posts';
+import { ALL_CALCULATORS, CALCULATOR_BY_SLUG } from '../data/calculators';
+import { editions, getEdition } from '../data/ledger';
+import { buildOgImageUrl, defaultOgImage } from '../lib/ogUrl';
+
+const SITE = 'https://www.viafidelitatis.com';
+const SITE_NAME = 'Via Fidelitatis';
+const DEFAULT_OG = defaultOgImage();
+const AUTHOR = 'Dustin Himmerich';
